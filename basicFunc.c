6fb5cbd0d0a0a13387cefc4af6f2ccb99931bcec
MangoCode0 <malik.asgar@outlook.com>
2024-08-17 15:45:19 +0530

    modify file basicFunc.c

--- basicFunc.c
+++ basicFunc.c
@@ -1,31 +1,31 @@
-// # include<stdio.h>
-// void greet (){
-//     printf ("good morning\n");
-//     printf ("how are you\n");
-//     return;
-// }
-// int main(){
-//   greet ();
-//   greet();
-//   return 0;  
-// }
+# include<stdio.h>
+void greet (){
+    printf ("good morning\n");
+    printf ("how are you\n");
+    return;
+}
+int main(){
+  greet ();
+  greet();
+  return 0;  
+}
 
-// # include<stdio.h>
-// void england(){
-//   printf("you are in england\n");
-//   return;
-// }
-// void australia(){
-//   printf("you are in australia\n");
-//   america();
-//   return;
-// }
-// void america(){
-//   printf("you are in america\n");
-//   england();
-//   return;
-// }
-// int main (){
-//   america();
-//   return 0;
-// }
+# include<stdio.h>
+void england(){
+  printf("you are in england\n");
+  return;
+}
+void australia(){
+  printf("you are in australia\n");
+  america();
+  return;
+}
+void america(){
+  printf("you are in america\n");
+  england();
+  return;
+}
+int main (){
+  america();
+  return 0;
+}
